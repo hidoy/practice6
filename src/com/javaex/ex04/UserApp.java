191c446092a9b50f3cf9595d66ab678d416c2c23
SeinOh <seinohseinoh@gmail.com>
2024-03-28 17:47:52 +0900

    2024/3/28

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -6,12 +6,12 @@ public class UserApp {
 		User u1[]=new User[3]; 
 		u1[0]= new Customer("jws","j1234","정우성",1000);
 		u1[1]= new Customer("yjs","y2345","이효리",2000);
-		u1[2]= new Employee("master","m7788","운영자",500);
+		u1[2]= new Employee("master","m7788","운영자",500000000);
 		
 		for(int i=0;i<3;i++) {
 			u1[i].showInfo();
 		}
-		System.out.printf("운영자의 월급은 %d",5000000);
+		System.out.printf("운영자의 월급은 %d",((Employee)u1[2]).getSalary());
 	}
 
 }
--- src/com/javaex/ex05/Depart.java
+++ src/com/javaex/ex05/Depart.java
@@ -6,6 +6,12 @@ public class Depart extends Employee{
 		super(name,salary);
 		this.department=department;
 	}
+	public String setDepartment() {
+		return department;
+	}
+	public void getDepartment(String department) {
+		this.department=department;
+	}
 	@Override
 	public void showInformation() {
 		System.out.printf("이름:%s 연봉:%d, 부서:%s",super.getName(),super.getSalary(),department);
